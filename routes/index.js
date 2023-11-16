73ba067b17a58e3ded240c4ed4097011c85871d9
rangga nurhadiansyah <ranggadiningrat7@gmail.com>
2023-11-16 17:01:59 +0700

    feat: router and controller payment

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const errorHandler = require('../middlewares/errorHandler');
 
 router.use('/users', require('./usersRoutes/userRouter'));
-
+router.use('/payment', require('./paymentRoutes/paymentRouter'));
 router.use(errorHandler);
 
 module.exports = router;
