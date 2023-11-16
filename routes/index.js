b3fe5124b8c18872df2a8b5d662d1adc2c725d7e
rangga nurhadiansyah <ranggadiningrat7@gmail.com>
2023-11-16 16:42:11 +0700

    feat: router and controller user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const errorHandler = require('../middlewares/errorHandler');
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hallo from LiveCharity Server'});
-});
+router.use('/users', require('./usersRoutes/userRouter'));
 
 router.use(errorHandler);
 
