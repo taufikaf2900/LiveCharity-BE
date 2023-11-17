e842c5db1efacf01e794cfc580133364bd92459c
rangga nurhadiansyah <ranggadiningrat7@gmail.com>
2023-11-17 22:20:34 +0700

    fix: payment history

--- controllers/livestreamControllers/livestreamController.js
+++ controllers/livestreamControllers/livestreamController.js
@@ -1,4 +1,4 @@
- class LivestreamController{
+class LivestreamController {
   static async handleStatusLivestream(req, res, next) {
     try {
       res.status(200).json({ message: 'This status livestream' });
@@ -7,13 +7,13 @@
     }
   }
 
- static async handleUpdateStatusLivestream(req, res, next) {
+  static async handleUpdateStatusLivestream(req, res, next) {
     try {
       res.status(200).json({ message: 'Update status livestream' });
     } catch (err) {
       next(err);
     }
   }
-};
+}
 
-module.exports = LivestreamController
+module.exports = LivestreamController;
