0cb8c09c174014cbc4a7eeeb6c162e279d71574d
rangga nurhadiansyah <ranggadiningrat7@gmail.com>
2023-11-19 19:42:59 +0700

    feat: read balance user

--- routes/livestreamRoutes/livestreamRoute.js
+++ routes/livestreamRoutes/livestreamRoute.js
@@ -4,5 +4,5 @@ const authentication = require('../../middlewares/authentication');
 
 router.patch('/:livestreamId/status', LivestreamController.handleUpdateStatusLivestream);
 router.post('/donate', authentication, LivestreamController.handleLivestreamDonate);
-
+router.get('/balance', authentication, LivestreamController.handleBalance);
 module.exports = router;
