953587474bcdf29cf04e6ef19be0a4483893be0d
Taufik Akbar Firmansyah <taufikaf2900@gmail.com>
2023-11-22 13:30:36 +0700

    feat: create feature live donation

--- routes/campaignRouter/campaignRouter.js
+++ routes/campaignRouter/campaignRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const CampaignController = require('../../controllers/campaignControllers/campaignController');
 const authentication = require('../../middlewares/authentication');
+const upload = require('../../middlewares/multer');
 
 router.get('/', CampaignController.handleCampaign);
 router.get('/pagenation', CampaignController.handleCampaignPagenation);
 router.get('/pagenation/users', authentication, CampaignController.handleCampaignPagenationUser);
 router.get('/:livestreamId', CampaignController.handleCampaignDetail);
-router.post('/', authentication, CampaignController.handleCampaignAdd);
+router.post('/', [authentication, upload.single('image')], CampaignController.handleCampaignAdd);
 module.exports = router;
